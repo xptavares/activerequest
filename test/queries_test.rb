ff1a0d36797d647df49fea958e7fbe85eb73faa1
Alexandre Tavares <xptavares@gmail.com>
2016-09-16 18:30:01 -0300

    fix tests

--- test/queries_test.rb
+++ test/queries_test.rb
@@ -2,22 +2,28 @@ require 'test_helper'
 
 class QueriestTest < Minitest::Test
   def test_should_has_two_blogs
-    assert_equal Blog.all.count, 2
+    VCR.use_cassette('all_blogs') do
+      assert_equal Blog.all.count, 2
+    end
   end
 
   def test_should_fill_id_after_create
-    blog = Blog.new title: "Test"
-    assert_equal blog.save, true
-    refute_nil blog.id
+    VCR.use_cassette('post_to_blogs') do
+      blog = Blog.new title: "Test"
+      assert_equal blog.save, true
+      refute_nil blog.id
+    end
   end
 
   def test_shouldnt_update_id_after_update_model
-    blog = Blog.first
-    string_to_udpate_title = "Update"
-    blog.title = string_to_udpate_title
-    id = blog.id
-    assert_equal blog.save, true
-    assert_equal blog.id, id
-    assert_equal blog.title, string_to_udpate_title
+    VCR.use_cassette('test_shouldnt_update_id_after_update_model') do
+      blog = Blog.first
+      string_to_udpate_title = "Update"
+      blog.title = string_to_udpate_title
+      id = blog.id
+      assert_equal blog.save, true
+      assert_equal blog.id, id
+      assert_equal blog.title, string_to_udpate_title
+    end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,3 +7,11 @@ require './examples/post.rb'
 require './examples/config.rb'
 
 require 'minitest/autorun'
+require "vcr"
+require "webmock"
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'test/cassettes'
+  c.hook_into :webmock
+  c.default_cassette_options = { serialize_with: :json }
+end
